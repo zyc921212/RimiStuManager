2d42828d5d56c4b9e43903a38e74edc15a36c0dd
msf <731007365@qq.com>
2017-08-24 18:32:48 +0800

    更新登录2.0
    更新了验证码功能。

--- SSM2/src/main/java/com/rimi/controller/UserController.java
+++ SSM2/src/main/java/com/rimi/controller/UserController.java
@@ -23,73 +23,83 @@ public class UserController {
 
 	@RequestMapping("/index.do")
 	public String jump(String userLoginName, String userPs, HttpServletRequest request, HttpServletResponse response,
-			HttpSession session, String remember) {
-		Cookie[] coos = request.getCookies();
-		if (coos == null) {
-			return "page-login";
-		} else {
-			return login(userLoginName, userPs, request, response, session, remember);
-		}
+			HttpSession session) {
+			return login(userLoginName, userPs, request, response, session);
 	}
 
 	@RequestMapping("/login.do")
 	public String login(String userLoginName, String userPs, HttpServletRequest request, HttpServletResponse response,
-			HttpSession session, String remember) {
-		if (userLoginName == null || userLoginName.equals("")) {
-			Cookie[] coos = request.getCookies();
-			if (coos == null) {
-				return "page-login";
-			} else {
-				for (int i = 0; i < coos.length; i++) {
-					if (coos[i].getName().equals("userLoginName")) {
-						userLoginName = coos[i].getValue();
-					}
-					if (coos[i].getName().equals("userPs")) {
-						userPs = coos[i].getValue();
-					}
+			HttpSession session) {
+		String code = (String)request.getSession().getAttribute("randCheckCode");
+		String checkCode = request.getParameter("checkCode");
+		Cookie[] coos = request.getCookies();
+		if (coos == null) {
+			return "page-login";
+		} else {
+			for (int i = 0; i < coos.length; i++) {
+				if (coos[i].getName().equals("userLoginName")) {
+					userLoginName = coos[i].getValue();
+					checkCode = (String)request.getSession().getAttribute("randCheckCode");
 				}
-				if (userLoginName == null || userLoginName.equals("")) {
-					return "page-login";
-				} else {
-					return login(userLoginName, userPs, request, response, session, remember);
+				if (coos[i].getName().equals("userPs")) {
+					userPs = coos[i].getValue();
 				}
 			}
+			if (userLoginName == null || userLoginName.equals("") || userPs == null || userPs.equals("")) {
+				return "page-login";
+			}
+		}
+		if (userLoginName == null || userLoginName.equals("")) {
+			request.setAttribute("message", "用户名不能为空 ！");
+			return "page-login";
 		} else {
-			UserBean ub = us.login(userLoginName, userPs);
-			if (ub == null) {
-				request.setAttribute("nameIsOk", "用户名不存在！");
+			if (checkCode == null || checkCode.equals("") ) {
+				request.setAttribute("userLoginName", userLoginName);
+				return "page-login";
+			} else if(!code.equals(checkCode)) {
+				request.setAttribute("message", "验证码错误 ！");
+				request.setAttribute("userLoginName", userLoginName);
 				return "page-login";
 			} else {
-				if (!ub.getUserPs().equals(userPs)) {
-					request.setAttribute("psIsOk", "密码错误！");
-					request.setAttribute("userLoginName", userLoginName);
+				UserBean ub = us.login(userLoginName, userPs);
+				if (ub == null) {
+					request.setAttribute("nameIsOk", "用户名不存在！");
 					return "page-login";
 				} else {
-					if (ub.getUserState() == 2 || ub.getUserJobState() == 3) {
-						request.setAttribute("userState", "该用户已经离职或者被禁用，无法登录 ！");
+					if (!ub.getUserPs().equals(userPs)) {
+						request.setAttribute("psIsOk", "密码错误！");
+						request.setAttribute("userLoginName", userLoginName);
 						return "page-login";
 					} else {
-						try {
-							if (remember.equals("Checked")) {
-								Cookie userLoginNameCookie = new Cookie("userLoginName", userLoginName);
-								Cookie userPsCookie = new Cookie("userPs", userPs);
-								userLoginNameCookie.setMaxAge(2 * 60);
-								userPsCookie.setMaxAge(2 * 60);
-								response.addCookie(userLoginNameCookie);
-								response.addCookie(userPsCookie);
-							}
-						} catch (Exception e) {
-							// TODO: handle exception
-						}
-						if (ub.getUserRole() == 1) {
-							session.setAttribute("ub", ub);
-							return "user/index";
-						} else if (ub.getUserRole() == 2) {
-							session.setAttribute("ub", ub);
-							return "stu/index";
-						} else {
-							request.setAttribute("userState", "身份信息错误，无法登录 ！");
+						if (ub.getUserState() == 2 || ub.getUserJobState() == 3) {
+							request.setAttribute("message", "该用户已经离职或者被禁用，无法登录 ！");
 							return "page-login";
+						} else {
+							try {
+								String remember = request.getParameter("remember");
+								if (remember.equals("Checked")) {
+									Cookie userLoginNameCookie;
+									Cookie userPsCookie;
+									userLoginNameCookie = new Cookie("userLoginName", userLoginName);
+									userPsCookie = new Cookie("userPs", userPs);
+									userLoginNameCookie.setMaxAge(1 * 60);
+									userPsCookie.setMaxAge(1 * 60);
+									response.addCookie(userLoginNameCookie);
+									response.addCookie(userPsCookie);
+								}
+							} catch (Exception e) {
+								// TODO: handle exception
+							}
+							if (ub.getUserRole() == 1) {
+								session.setAttribute("ub", ub);
+								return "user/index";
+							} else if (ub.getUserRole() == 2) {
+								session.setAttribute("ub", ub);
+								return "stu/index";
+							} else {
+								request.setAttribute("message", "身份信息错误，无法登录 ！");
+								return "page-login";
+							}
 						}
 					}
 				}

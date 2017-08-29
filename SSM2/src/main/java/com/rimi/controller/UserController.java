1b8bff49409ea319d9561514f2001b107a94f8b2
msf <731007365@qq.com>
2017-08-29 09:44:49 +0800

    登录更新2.1
    更新备份了一下

--- SSM2/src/main/java/com/rimi/controller/UserController.java
+++ SSM2/src/main/java/com/rimi/controller/UserController.java
@@ -7,12 +7,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.rimi.bean.UserBean;
 import com.rimi.service.UserService;
+
 
 @Controller
 @SessionAttributes("ub")
@@ -20,7 +20,9 @@ public class UserController {
 
 	@Autowired
 	UserService us;
-
+	
+	Md5 md5 = new Md5();
+	
 	@RequestMapping("/index.do")
 	public String jump(String userLoginName, String userPs, HttpServletRequest request, HttpServletResponse response,
 			HttpSession session) {
